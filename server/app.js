7270bcb5f20a4e36c47aa808759d1e13cae0935d
AnthonyClausing <anthonyclausing@gmail.com>
2018-09-30 21:03:54 -0400

    main function finished, presentation next

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const riot = require('../riot_api_key');
 function riotURLCreator(type, key, params){
     switch(type){
     case 'champions':
-      return `https://na1.api.riotgames.com/lol/static-data/v3/champions?locale=en_US&champListData=image&tags=tags&api_key=${key}`;
+      return `http://ddragon.leagueoflegends.com/cdn/8.18.1/data/en_US/champion.json`;
     case 'summoner':
       return `https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/${params.name}?api_key=${key}`;
     case 'matches':
@@ -14,6 +14,15 @@ function riotURLCreator(type, key, params){
       return `https://na1.api.riotgames.com/lol/match/v3/matches/${params.gameId}?api_key=${key}`
     }
 }
+app.use(function(req, res, next) {
+  res.set({
+    "Access-Control-Allow-Origin" : "*",
+    "Access-Control-Allow-Headers" : "Origin, X-Requested-With, content-type, Accept"
+  });
+  app.disable('x-powered-by');
+  next();
+ });
+ 
 app.get('/api/champions', function(req,res){ 
  request(riotURLCreator('champions', riot.key), (err,response,body) =>{
    !err ?  res.send(JSON.parse(response.body)) : console.log(err);
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,9 +3,10 @@
     <div align="center">
       <h1>Hello</h1>
         <ul>
-         <li v-for="champ in Champions" :key="champ.id">
-           <h5>{{champ.name}}</h5>
-            <img :src="`https://ddragon.leagueoflegends.com/cdn/8.7.1/img/champion/${champ.image.full}`"/>
+           <li v-for="answer in getAnswer" :key="answer.champ.id">
+           <h5>{{answer.champ.name}}</h5>
+            <img :src="`https://ddragon.leagueoflegends.com/cdn/8.18.1/img/champion/${answer.champ.image.full}`"/>
+            <h2>{{answer.count}}</h2>    
           </li>
         </ul>
       </div>
@@ -16,58 +17,78 @@
 import axios from 'axios';
 import querystring from 'querystring';
 import riot from '../../riot_api_key';
+import _ from 'lodash';
 export default {
   name: 'HelloWorld',
   data () {
     return {
       User : "",
-      Champions: {}
+      Champions: {},
+      Answer: {}
+    }
+  },
+  computed:{
+    getAnswer(){
+      let newAnswer = []
+      for(let id in this.Answer){
+        // console.log(this.Answer[id], 'this.answer at id')
+        //console.log(id,'inside getanswer computed')
+        let count = this.Answer[id]
+        let champ = _.find(this.Champions, c => c.key === id)
+        newAnswer.push({champ,count})
+      }
+      return _.orderBy(newAnswer,["count"],["desc"]).slice(0,5)
     }
   },
   mounted(){
-      axios.get('http://localhost:3000/api/summoner?name=himay85')
-      .then( res => {
-        axios.get(`http://localhost:3000/api/matches?accountId=${res.data.accountId}`)
+      // TODO Turn below axios calls into state actions 
+      axios.get('http://localhost:3000/api/champions')
+        .then(res => {
+          this.Champions = res.data.data
+        })
+        .catch(err => console.log(error,'from HW.vue'))
+      axios.get('http://localhost:3000/api/summoner?name=Blitzkreeg8')
+        .then( res => {
+          axios.get(`http://localhost:3000/api/matches?accountId=${res.data.accountId}`)
         .then(  res => {
           return this.getMatchListInfo(res.data.matches)
         })
         .then( newMatchList => {
-          console.log(this.getChampCounts(newMatchList,'Himay85'));
+          this.Answer = this.getChampCounts(newMatchList,'Blitzkreeg8')
         })
       })
-      .catch(err => console.error(err))
+        .catch(err => console.error(err))
   },
   methods:{
     getMatchListInfo: async function(matchList){
       let newMatchList = [];
       for(let match of matchList) {
-        let whatever =  await axios.get(`http://localhost:3000/api/match?gameId=${match.gameId}`)
-        newMatchList.push(whatever.data)
+        let matchData =  await axios.get(`http://localhost:3000/api/match?gameId=${match.gameId}`)
+        newMatchList.push(matchData.data)
       }
       return newMatchList;
     },
     //can add another parameter like a bool for checking for bad allies putting it in the if block
     getChampCounts : function(games,name){
-      let champCountObj = {};
-      //console.log(games)
+      let championCounter = {};
       games.forEach( game => {
       let partId = game.participantIdentities.find( participant => participant.player.summonerName === name).participantId;
-      console.log(partId, 'partId')
       let enemyTeam;
       let win = game.participants[partId - 1].stats.win;
       if(!win){
-        enemyTeam = partId >= 5 ? game.participants.slice(5,10) : game.participants.slice(0,5);
-        console.log(enemyTeam)
+        enemyTeam = partId >= 5 ? game.participants.slice(0,5) : game.participants.slice(5,10);
         enemyTeam.forEach( player => {
-         if(!champCountObj[player.championId]){
-            champCountObj[player.championId] = 1;
-         }else{
-            champCountObj[player.championId]++;
-         }
+          if(!championCounter[player.championId]){ 
+            championCounter[player.championId] = 1;
+          }else{
+            championCounter[player.championId]++
+          }
         })
       }
       })
-      return champCountObj;
+
+     console.log(championCounter)
+      return championCounter;
     }
   }
 }
